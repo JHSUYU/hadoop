c2fa5207f20c871024f910ecd5a60941d343d78f
李振羽 <181250079@smail.nju.edu.cn>
2024-02-12 17:38:43 -0500

    add verify rule1

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DataStreamer.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DataStreamer.java
@@ -734,8 +734,8 @@ class DataStreamer extends Daemon {
       try {
         // process datanode IO errors if any
         LOG.info("Before shadowErrorHandler, the nodes are: {}", Arrays.toString(this.nodes));
-//        DatanodeInfo[] originalNodes = new DatanodeInfo[this.nodes.length];
-//        System.arraycopy(this.nodes, 0, originalNodes, 0, this.nodes.length);
+        DatanodeInfo[] originalNodes = new DatanodeInfo[this.nodes.length];
+        System.arraycopy(this.nodes, 0, originalNodes, 0, this.nodes.length);
         boolean doSleep = false;
 //        if (!checker()) {
 //          doSleep = shadowProcessDatanodeOrExternalError();
