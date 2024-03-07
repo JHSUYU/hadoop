19f74ac6301ebcd4aed82757471995a8abd8acca
Zhenyu Li <893652269@qq.com>
2024-03-06 23:23:30 -0500

    test rule1

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DataStreamer.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DataStreamer.java
@@ -2385,12 +2385,16 @@ class DataStreamer extends Daemon {
         Status pipelineStatus = resp.getStatus();
         firstBadLink = resp.getFirstBadLink();
 
-        shadowDataStreamer.prepareForSender(blockCopy, nodeStorageTypes[0], accessToken,
-                dfsClient.clientName, nodes, nodeStorageTypes, null, bcs,
-                nodes.length, block.getNumBytes(), bytesSent, newGS,
-                checksum4WriteBlock, cachingStrategy.get(), isLazyPersistFile,
-                (targetPinnings != null && targetPinnings[0]), targetPinnings,
-                nodeStorageIDs[0], nodeStorageIDs, this);
+        try {
+          shadowDataStreamer.prepareForSender(blockCopy, nodeStorageTypes[0], accessToken,
+                  dfsClient.clientName, nodes, nodeStorageTypes, null, bcs,
+                  nodes.length, block.getNumBytes(), bytesSent, newGS,
+                  checksum4WriteBlock, cachingStrategy.get(), isLazyPersistFile,
+                  (targetPinnings != null && targetPinnings[0]), targetPinnings,
+                  nodeStorageIDs[0], nodeStorageIDs, this);
+        }catch (IOException e){
+          e.printStackTrace();;
+        }
 
         LOG.info("Failure Recovery 2403");
 
