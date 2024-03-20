a396b6ad5afed9e0a2e3d3e28c4038da12b6f7a6
Zhenyu Li <893652269@qq.com>
2024-03-20 16:48:05 -0400

    fix bug

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/BlockReceiver.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/BlockReceiver.java
@@ -433,13 +433,14 @@ class BlockReceiver implements Closeable {
 //      assert streams != null : "null streams!";
 //
 //      // read checksum meta information
+       this.needsChecksumTranslation = false;
 //      this.clientChecksum = requestedChecksum;
 //      this.diskChecksum = streams.getChecksum();
 //      this.needsChecksumTranslation = !clientChecksum.equals(diskChecksum);
 //      this.bytesPerChecksum = diskChecksum.getBytesPerChecksum();
 //      this.checksumSize = diskChecksum.getChecksumSize();
-//       this.bytesPerChecksum = 0;
-//       this.checksumSize = 0;
+       this.bytesPerChecksum = 0;
+       this.checksumSize = 0;
 //
 //      this.checksumOut = new DataOutputStream(new BufferedOutputStream(
 //              streams.getChecksumOut(), DFSUtilClient.getSmallBufferSize(
